1463890ab4a6fb8a2e6995263cbcf683822c0e4c
AP <Alexanderp2026@student.cis.edu.hk>
2025-10-07 15:48:23 +0800

    hi

--- src/main/java/org/example/RLE.java
+++ src/main/java/org/example/RLE.java
@@ -26,12 +26,53 @@ public class RLE {
     }
 
     /** TODO 1: Given a String (a genome sequence of COVID-19) implement the RLE algorithm that will use RLE to compress a String. Returns the compressed String. */
-    public static String compress(String uncompressed) { return null; }
+    public static String compress(String uncompressed) {if ( uncompressed.isEmpty() || uncompressed == null){
+        return null;
+
+    }
+        StringBuilder str = new StringBuilder();
+        int count = 1;
+
+        for (int i = 1; i <= uncompressed.length(); i++) {
+
+            if (i == uncompressed.length() || uncompressed.charAt(i) != uncompressed.charAt(i - 1)) {
+                str.append(count);
+                str.append(uncompressed.charAt(i - 1));
+                count = 1;
+            } else {
+                count++;
+            }
+        }
+
+        return str.toString();}
 
     /** TODO 2: Given a String (a genome sequence of COVID-19) implement the RLE algorithm that will use RLE to decompress a String. Returns the uncompressed String. */
     public static String decompress(String compressed) {
-        return null;
-    }
+        if (compressed.isEmpty() || compressed == null){
+            return null;
+        }
+        StringBuilder str = new StringBuilder();
+
+        int i = 0;
+
+        while (i < compressed.length()) {
+            StringBuilder countBuilder = new StringBuilder();
+            while (i < compressed.length() && Character.isDigit(compressed.charAt(i))) {
+                countBuilder.append(compressed.charAt(i));
+                i++;
+            }
+
+            if (i < compressed.length()) {
+                char character = compressed.charAt(i);
+                i++;
+                int count = Integer.parseInt(countBuilder.toString());
+                for (int j = 0; j < count; j++) {
+                    str.append(character);
+                }
+            }
+        }
+
+        return str.toString();    }
 
 
 }
